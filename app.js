fa40418b6296c6cc579cf1a70082868b25e72c17
Raman Abdulla <raman.abdulla99@outlook.de>
2020-02-05 10:37:16 +0100

    Can now import own modules

---
+++ app.js
@@ -0,0 +1,34 @@
+// Import libaries
+const express = require('express');
+const socket = require('socket.io');
+const mysql = require('mysql');
+const colors = require('colors');
+const port = 3000;
+
+// App Setup
+var app = express().use(express.static('client/'));
+var server = app.listen(port,function() {
+    console.log('::: Started Server at Port: '.gray + port.toString().gray);
+});
+
+// SQL Database
+/*
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "tanasia"
+})
+*/
+
+// Socket Setup
+var io = socket(server);
+
+// Export Modules
+module.exports = {
+    // db,
+    io
+};
+
+// Require Modules
+require('./server/chat.js');
---
+++ server/chat.js
@@ -0,0 +1,12 @@
+// Import modules
+const app = require('./../app.js');
+const mysql = require('mysql');
+const filename = 'socket.js';
+
+// Console Output
+console.log(("::: Initalized "+ filename).gray);
+
+app.io.on('connection', function(socket) {
+    // Client connects
+    console.log("New Client ".brightGreen + socket.id)
+});
